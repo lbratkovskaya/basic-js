8bd8ea552b45716e2b3c7ddf419978f75715ac03
lbratkovskaya <lbratkovskaya@naumen.ru>
2020-09-26 22:58:20 +0500

    init: first 4 tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam( members ) {
+  if (!Array.isArray(members))
+  {
+    return false;
+  }
+  let lettersArray = [];
+  members.forEach( member => 
+    {
+      if ( typeof member === 'string' && member !== '')
+      {
+        member = member.trim();
+        let firstLetter = member.substring(0,1).toUpperCase();
+
+        if (lettersArray.length == 0)
+        {
+          lettersArray.push(firstLetter);
+        }
+        else if (lettersArray.indexOf(firstLetter) > -1)
+        {
+          lettersArray.splice(lettersArray.indexOf(firstLetter), 0, firstLetter);
+        }
+        else {
+          let index = 0;
+          let letter= lettersArray[0];
+          while ( letter < firstLetter)
+          {
+            letter = lettersArray[++index];
+          }
+
+          if (index == lettersArray.length)
+          {
+            lettersArray.push(firstLetter);
+          }
+          else
+          {
+            lettersArray.splice(index, 0, firstLetter);
+          }
+        }
+      }
+    });
+
+    return lettersArray.join('');
 };
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options, defaultSeparator) {
+  str = '' + str;
+
+  if (!defaultSeparator) {
+    defaultSeparator = '+';
+  }
+
+  let result = '';
+  let additionalStr = options['additionRepeatTimes']
+    ? repeater(options['addition'], { repeatTimes: options['additionRepeatTimes'], separator: options['additionSeparator'] }, '|')
+    : options['addition'] || '';
+
+  if (!options['repeatTimes']) {
+    return str + additionalStr;
+  }
+
+  for (i = 0; i < options['repeatTimes']; i++) {
+
+    result += str + additionalStr
+      + (i < options['repeatTimes'] - 1 ? (options['separator'] ? options['separator'] : defaultSeparator) : '');
+  }
+
+  return result;
 };
-  
