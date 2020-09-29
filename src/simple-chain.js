854e90a0a9f5c84710c846791647b880950aa5fd
lbratkovskaya <lbratkovskaya@naumen.ru>
2020-09-29 11:46:45 +0500

    feat: solve all the tasks

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,25 +2,39 @@ const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if ( value === undefined ) {
+      this.chain.push('(  )');
+    }
+    else {
+      this.chain.push('( ' + value + ' )');
+    }
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof position !== 'number' || position > this.chain.length)
+    {
+      this.chain = [];
+      throw new Error('Invalid position');
+    }
+    this.chain.splice(position - 1, 1);
+
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let result = Array.from(this.chain).join ('~~');
+    this.chain = [];
+
+    return result;
   }
 };
+
+chainMaker.chain = [];
 
 module.exports = chainMaker;
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason( date ) {
+
+  if ( !date ) {
+    return 'Unable to determine the time of year!'
+  }
+
+  if (typeof date.getMilliseconds() === 'function')
+  {
+    throw new Error('Not a date actually!');
+  }
+
+  let getClassOf = Function.prototype.call.bind(Object.prototype.toString)
+
+  if (!getClassOf(date).includes('Date'))
+  {
+    throw new Error('You almost caught me!');
+  }
+
+  switch (Math.floor( (date.getMonth() + 1) / 3))
+  {
+    case 0:
+    case 4: return 'winter';
+    case 1: return 'spring';
+    case 2: return 'summer';
+    case 3: return 'autumn';
+    default: return 'Not a valid date';
+  }
 };
